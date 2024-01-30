c2f9c5b6280d4498403173607f9facbc2e7c3c39
wjbetech <wjbetech@gmail.com>
2024-01-30 13:15:32 +0900

    initial storybook, emotion, vanilla setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const {
+    createVanillaExtractPlugin
+  } = require('@vanilla-extract/next-plugin');
+  const withVanillaExtract = createVanillaExtractPlugin();
+  
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {};
+  
+  module.exports = withVanillaExtract(nextConfig);
